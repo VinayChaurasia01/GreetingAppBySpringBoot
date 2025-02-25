8429adb5070e65bca50a3ead643ccb21f43cea6c
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-02-25 16:08:32 +0530

    [Vinay] Add . Greeting Repository interface

--- GreetingApp/src/main/java/com/greetingapp/repository/GreetingRepository.java
+++ GreetingApp/src/main/java/com/greetingapp/repository/GreetingRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+
 }
